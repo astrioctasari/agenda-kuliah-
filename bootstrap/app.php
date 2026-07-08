1ff0c56f458e785c7d7cfba408fc2728ba9e4e94
Astri Octasari <astriococ@gmail.com>
2026-07-08 22:45:13 +0700

    Configure Sanctum middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,33 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+  ->withMiddleware(function (Middleware $middleware): void {
+    $middleware->statefulApi();
+})
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
+
+    use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthenticated.'
+                ], 401);
+            }
+        });
+    })
+    ->create();
